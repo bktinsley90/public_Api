bc4abc34677eaa0f311ed498c19cd2b0823b0a4b
Brittany Tinsley <bktinsley90@gmail.com>
2021-03-29 11:40:50 -0400

    modals updated and added color

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # public_Api
  
+Project Overview: This is TechDegree's 5th project. This takes in 12 random users from randomuser Api and displays data to the page. I was able to update the CSS to add the color but I was not able to figure out how to toggle between modals. The search bar is displayed on the page but currently does not work. Lastly I will need to work on refactoring the code to look more consise and clean. The main functionality for the project works as intended.
+
+Skills:
+javascript, CSS 
+
+Future updates
+Search feature
+Modal Toggle
--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ let body = document.querySelector('body')
 
 /**
  * Fetch Functions
+ * I went with async/await to me its much simpler to understand and does more work for the promises
  */
 
 const fetchData = async () => {
@@ -23,12 +24,22 @@ const fetchData = async () => {
 
 }
 fetchData()
+
+/**
+ * Search Bar 
+ */
+const searchDiv = document.querySelector('.search-container')
+const searchBar = `  <form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>`
+searchDiv.insertAdjacentHTML('afterbegin', searchBar)
 /**
  * Helper Functions
  * @params {data} takes in the data from API and using template literals to generate cards
  */
 
-function generateCard(data, index) {
+function generateCard(data) {
     const galleryHTML =
         `<div class="card">
         <div class="card-img-container">
@@ -47,10 +58,11 @@ function generateCard(data, index) {
 }
 /**
  * Modal windows- create the modal then use event listener to activate
+ * @params {data} {i} data from the api, and the index for each user 
  */
-
 function createModal(data, i) {
-    console.log(data, i)
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#to_get_date_month_and_year_or_time
+    let birthday = new Date(data.dob.date).toLocaleDateString("en-US").split("/") 
     // Create modal
     const openModal = `
         <div class="modal-container">
@@ -58,13 +70,17 @@ function createModal(data, i) {
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
                 <img class="modal-img" src="${data.picture.thumbnail}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${data.name.first}</h3>
+                <h3 id="name" class="modal-name cap">${data.name.first}${data.name.last}<h3>
                 <p class="modal-text">${data.email}</p>
-                <p class="modal-text cap">${data.location.city}city</p>
+                <p class="modal-text cap">${data.location.city}</p>
                 <hr>
-                <p class="modal-text">${data.cell}(555) 555-5555</p>
+                <p class="modal-text">${data.cell}</p>
                 <p class="modal-text">${data.location.street.number} ${data.location.street.name},${data.location.city} ${data.location.state} ${data.location.postcode}</p>
-                <p class="modal-text">Birthday: ${data.dob}</p>
+                <p class="modal-text">Birthday: ${birthday[0]+"/"+birthday[1]+"/"+birthday[2]} </p>
+            </div>
+            <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
             </div>
         </div>`
     body.insertAdjacentHTML('afterbegin', openModal)
@@ -77,8 +93,9 @@ function createModal(data, i) {
     cards[i].addEventListener('click', () => {
         modalContainer.style.display='block'
     })
-    const btn = document.querySelector('button')
-    btn.addEventListener('click', () => {
+    const closeBtn = document.querySelector('.modal-close-btn')
+    closeBtn.addEventListener('click', () => {
         modalContainer.style.display='none'
     })
+
 }
