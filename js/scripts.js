b561192bed5fd30d0054773dbd63d6d28c0eeb41
Brittany Tinsley <bktinsley90@gmail.com>
2021-03-28 20:00:34 -0400

    generates users and modal onclick

--- js/scripts.js
+++ js/scripts.js
@@ -3,73 +3,82 @@
  */
 let galleryDiv = document.getElementById('gallery')
 let body = document.querySelector('body')
-let cards = document.querySelectorAll('.card')
+
 /**
  * Fetch Functions
  */
-fetchData('https://randomuser.me/api/?results=12')
-function fetchData(url) {
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => generateCard(data.results))
-        .catch(error => console.log('Pump your breaks Error'))
-}  
 
+const fetchData = async () => {
+    try {
+        const res = await fetch('https://randomuser.me/api/?results=12');
+        const data = await res.json()
+        const users = data.results
+        for (let i = 0; i < users.length; i++) {
+            generateCard(users[i])
+            createModal(users[i], i)
+        }
+    } catch (e) {
+        console.log('Pump your breaks Error', e)
+    }
+
+}
+fetchData()
 /**
  * Helper Functions
  * @params {data} takes in the data from API and using template literals to generate cards
  */
 
-function generateCard(data) {
-    console.log(data)
-    data.forEach(item => {
-        const galleryHTML = 
+function generateCard(data, index) {
+    const galleryHTML =
         `<div class="card">
         <div class="card-img-container">
-            <img class="card-img" src="${item.picture.large}" alt="profile picture">
+            <img class="card-img" src="${data.picture.large}" alt="profile picture">
         </div>
         <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
-            <p class="card-text">${item.email}</p>
-            <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
+            <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+            <p class="card-text">${data.email}</p>
+            <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
         </div>
         </div>`
 
-        galleryDiv.insertAdjacentHTML('beforeend', galleryHTML)
-    });
+    galleryDiv.insertAdjacentHTML('beforeend', galleryHTML)
+
 
 }
 /**
  * Modal windows- create the modal then use event listener to activate
  */
 
-function createModal(e) {
-    e.map(item => {
-        const openModal =`
+function createModal(data, i) {
+    console.log(data, i)
+    // Create modal
+    const openModal = `
         <div class="modal-container">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${item.picture.small}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${item.name.first}</h3>
-                <p class="modal-text">${item.email}</p>
-                <p class="modal-text cap">${item.location.city}city</p>
+                <img class="modal-img" src="${data.picture.thumbnail}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${data.name.first}</h3>
+                <p class="modal-text">${data.email}</p>
+                <p class="modal-text cap">${data.location.city}city</p>
                 <hr>
-                <p class="modal-text">${item.cell}(555) 555-5555</p>
-                <p class="modal-text">${item.location.street.number} ${item.location.street.name},${item.location.city} ${item.location.state} ${item.location.postcode}</p>
-                <p class="modal-text">Birthday: ${item.dob}</p>
+                <p class="modal-text">${data.cell}(555) 555-5555</p>
+                <p class="modal-text">${data.location.street.number} ${data.location.street.name},${data.location.city} ${data.location.state} ${data.location.postcode}</p>
+                <p class="modal-text">Birthday: ${data.dob}</p>
             </div>
         </div>`
-        body.insertAdjacentHTML('afterbegin', openModal)
-    })
-      
- }
- /**
-  * Event Listener
-  * 
-  */
-//cards.addEventlistener('click', createModal())
+    body.insertAdjacentHTML('afterbegin', openModal)
 
-/**
- * Post Data
- */
+    const modalContainer = document.querySelector('.modal-container')
+    // set style display to none
+    modalContainer.style.display='none'
+    // addEventListner to unhide
+    let cards = document.querySelectorAll('.card')
+    cards[i].addEventListener('click', () => {
+        modalContainer.style.display='block'
+    })
+    const btn = document.querySelector('button')
+    btn.addEventListener('click', () => {
+        modalContainer.style.display='none'
+    })
+}
